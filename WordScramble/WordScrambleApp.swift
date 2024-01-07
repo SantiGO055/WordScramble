1aa24a7ea80d8776ae1527b176a1f1f82275ddeb
SantiGO055 <santigonzalez05@gmail.com>
2024-01-07 20:53:30 -0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Santiago Gonzalez on 07/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
